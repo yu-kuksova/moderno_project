fad890bb4a28731da351adb3785803171be37cd0
yu-kuksova <kuksova813@gmail.com>
2020-05-10 02:49:44 +0300

    added section products-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
-    
+
     var mixer = mixitup('.products__inner-box');
+
+    $(".rate-star").rateYo({
+        rating: 5.0,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.products-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
     
 });
