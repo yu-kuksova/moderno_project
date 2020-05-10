e4391e8ce298aa21eb97394f767c96fa060bc9e2
yu-kuksova <kuksova813@gmail.com>
2020-05-10 23:36:05 +0300

    adaptiv header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 $(function(){
 
-    var mixer = mixitup('.products__inner-box');
-
     $(".rate-star").rateYo({
         rating: 5.0,
         starWidth: "12px",
@@ -15,4 +13,15 @@ $(function(){
         slidesToScroll: 4,
     });
     
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+
+
+     var mixer = mixitup('.products__inner-box');
 });
